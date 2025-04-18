b063e18b56c6374d3174f35530416d3a26fa3ae1
MRC-Tech999 <emprerorsukuna@gmail.com>
2025-04-17 20:29:27 -0400

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -2,15 +2,16 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Succès</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="container">
-    <h1>Enregistré avec succès !</h1>
-    <p>Merci d'avoir rejoint notre base WhatsApp.</p>
-    <a href="index.html">Retour</a>
+    <h1>Contact enregistré avec succès !</h1>
+    <p>Tu peux maintenant télécharger le fichier VCF.</p>
+    <a href="download.php" class="btn">Télécharger les contacts</a><br>
+    <a href="index.html" class="btn">Retour</a>
   </div>
+  <a href="#" class="top-link">Retour en haut</a>
 </body>
 </html>
