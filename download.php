3135ca640cddc5c146c99c02c46f303a6b276b19
MRC-Tech999 <emprerorsukuna@gmail.com>
2025-04-17 20:57:42 -0400

    Update download.php

--- download.php
+++ download.php
@@ -1,36 +1,17 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $code = $_POST['code'];
-    if ($code === '2025') {
-        include 'generate_vcf.php';
-        header('Content-Type: text/vcard');
-        header('Content-Disposition: attachment; filename="contacts.vcf"');
-        readfile('contacts.vcf');
-        exit;
-    } else {
-        $error = "Mot de passe incorrect.";
-    }
+$vcf_file = 'contacts.vcf';
+$password = '2025';
+
+if (!isset($_GET['pw']) || $_GET['pw'] !== $password) {
+    die("Accès refusé.");
 }
-?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8" />
-  <title>Téléchargement sécurisé</title>
-  <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-  <div class="container">
-    <h1>Télécharger les contacts</h1>
-    <form method="POST">
-      <input type="password" name="code" placeholder="Mot de passe" required />
-      <button type="submit">Télécharger</button>
-    </form>
-    <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
-    <a href="index.html" class="btn">Retour</a>
-  </div>
+if (!file_exists($vcf_file)) {
+    die("Fichier introuvable.");
+}
 
-  <a href="#" class="top-link">Retour en haut</a>
-</body>
-</html>
+header('Content-Description: File Transfer');
+header('Content-Type: text/x-vcard');
+header('Content-Disposition: attachment; filename="contacts.vcf"');
+readfile($vcf_file);
+exit;
