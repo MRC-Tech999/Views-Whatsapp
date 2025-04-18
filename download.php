44912923bcc56544555dfe16686a461483c17e05
MRC-Tech999 <emprerorsukuna@gmail.com>
2025-04-17 20:29:58 -0400

    Update download.php

--- download.php
+++ download.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $code = $_POST['code'];
     if ($code === '2025') {
@@ -18,18 +16,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="UTF-8">
+  <meta charset="UTF-8" />
   <title>Téléchargement sécurisé</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css" />
 </head>
 <body>
   <div class="container">
-    <h1>Mot de passe requis</h1>
+    <h1>Télécharger les contacts</h1>
     <form method="POST">
-      <input type="password" name="code" placeholder="Entrer le mot de passe" required>
+      <input type="password" name="code" placeholder="Mot de passe" required />
       <button type="submit">Télécharger</button>
     </form>
     <?php if (isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+    <a href="index.html" class="btn">Retour</a>
   </div>
+
+  <a href="#" class="top-link">Retour en haut</a>
 </body>
 </html>
